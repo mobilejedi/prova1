fc910ca670c5013c810c7980719a8d01cb17375b
gmorelli <gianmarco.morelli@alten.it>
2015-12-13 23:15:33 +0100

    Initial Checkin

---
+++ prova1/prova1/src/main/java/prova/prova1/App.java
@@ -0,0 +1,13 @@
+package prova.prova1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World Giamma!" );
+    }
+}
